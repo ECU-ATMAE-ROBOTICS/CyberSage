fbdad2ba8ae1d9d1287b54e24f3b9b1a1bb2891b
JarredTD <jarred.desrosiers@yahoo.com>
2023-06-27 18:24:25 -0700

    Constant values

---
+++ src/util/constants.js
@@ -0,0 +1,14 @@
+//TODO Create a config page where some of these values can be easily changed
+module.exports = {
+    reactionEmoji: {
+        bulbEmoji: "💡",
+        computerEmoji: "💻",
+        toolsEmoji: "🛠️",
+    },
+
+    ID: {
+        setRoleMessageID: "1123120003047247952",
+        roleSelectionChannelID: "1123119173757849731",
+        serverID: "1106740376515125309",
+    },
+};
