8043d7bdb919f8040ccc37aafbf31bc047de8512
JarredTD <jarred.desrosiers@yahoo.com>
2023-06-28 17:47:54 -0700

    Remove unused file

--- src/util/logTimerUtil.js
+++
@@ -1,7 +0,0 @@
-// const constants = require("../util/constants");
-// const logger = constants.logger;
-
-// modules.exports = function restartTimer() {
-//     clearInterval(logger.logTimer);
-//     logger.logTimer = logger.startTimer;
-// };
