d01a74943726e052be361e7d7cae4ab1d8898c68
JarredTD <jarred.desrosiers@yahoo.com>
2024-04-13 16:30:53 -0400

    chore(logger): Scrap log combiner stratagy

--- logger/logger.js
+++ logger/logger.js
@@ -9,131 +9,67 @@ const { logLevels, loggerConfig } = require("../src/constants/loggerConsts");
  * @class Logger
  */
 class Logger {
-    /**
-     * Initializes the Logger with a file and console transport.
-     * @constructor
-     */
-    constructor() {
-        this.fileTransport = new FileTransport();
-        this.consoleTransport = new ConsoleTransport();
-    }
+  /**
+   * Initializes the Logger with a file and console transport.
+   * @constructor
+   */
+  constructor() {
+    this.fileTransport = new FileTransport();
+    this.consoleTransport = new ConsoleTransport();
+  }
 
-    /**
-     * Log a message with an optional log level.
-     * @param {string} message - The message to be logged.
-     * @param {string} logLevel - The log level being appended to the message. Default is INFO.
-     */
-    log(message, logLevel = logLevels.INFO) {
-        this.fileTransport.log(message, logLevel);
-        this.consoleTransport.log(message, logLevel);
-    }
+  /**
+   * Log a message with an optional log level.
+   * @param {string} message - The message to be logged.
+   * @param {string} logLevel - The log level being appended to the message. Default is INFO.
+   */
+  log(message, logLevel = logLevels.INFO) {
+    this.fileTransport.log(message, logLevel);
+    this.consoleTransport.log(message, logLevel);
+  }
 
-    /**
-     * Save the logs to a file. If an error occurs, it throws a LogSaveException.
-     */
-    saveToFile() {
-        try {
-            this.fileTransport.saveToFile();
-        } catch (error) {
-            throw new LogSaveException(
-                `Logger::saveToFile() Failed to save logs to ${this.fileTransport.fileName}`,
-                error
-            );
+  /**
+   * Save the logs to a file. If an error occurs, it throws a LogSaveException.
+   */
+  saveToFile() {
+    try {
+      this.fileTransport.saveToFile();
+    } catch (error) {
+      throw new LogSaveException(
+        `Logger::saveToFile() Failed to save logs to ${this.fileTransport.fileName}`,
+        error,
+      );
+    }
+  }
+
+  /**
+   * Start the timer to periodically save logs to a file and handle exceptions.
+   */
+  startLogWriterTimer() {
+    clearInterval(this.logWriterTimer);
+    this.logWriterTimer = setInterval(() => {
+      try {
+        this.saveToFile();
+        const currentDate = new Date().toISOString().split("T")[0];
+        if (currentDate !== this.fileTransport.logDate) {
+          this.fileTransport.logDate = currentDate;
+          this.fileTransport.fileName = `${loggerConfig.filePath}/${this.fileTransport.logDate}.txt`;
         }
-    }
-
-    /**
-     * Start the timer to periodically save logs to a file and handle exceptions.
-     */
-    startLogWriterTimer() {
-        clearInterval(this.logWriterTimer);
-        this.logWriterTimer = setInterval(() => {
-            try {
-                this.saveToFile();
-                const currentDate = new Date().toISOString().split("T")[0];
-                if (currentDate !== this.fileTransport.logDate) {
-                    this.fileTransport.logDate = currentDate;
-                    this.fileTransport.fileName = `${loggerConfig.filePath}/${this.fileTransport.logDate}.txt`;
-                }
-            } catch (error) {
-                this.startLogWriterTimer();
-                if (error instanceof LogSaveException) {
-                    this.consoleTransport.log(
-                        `Logger::startLogWriterTimer() Exception during saving the logs => ${error} => ${error.parentError}`,
-                        logLevels.ERROR
-                    );
-                } else {
-                    this.consoleTransport.log(
-                        `Logger::startLogWriterTimer() Exception occurred that isn't LogSaveException, possible exception leak => ${error}`,
-                        logLevels.ERROR
-                    );
-                }
-            }
-        }, loggerConfig.timerWriterInterval);
-    }
-
-    /**
-     * Start the timer to periodically combine log files by month/year.
-     */
-    //! Some kind of issue with a path not being found
-    startLogCombinerTimer() {
-        clearInterval(this.logCombinerTimer);
-        this.logCombinerTimer = setInterval(() => {
-            try {
-                this.combineLogsByMonthAndYear();
-            } catch (error) {
-                this.consoleTransport.log(
-                    `Logger::startLogCombinerTimer() Exception occurred => ${error}`,
-                    logLevels.ERROR
-                );
-            }
-        }, loggerConfig.timerCombinerInterval);
-    }
-
-    /**
-     * Combine log files in the same month and year into a single file.
-     */
-    combineLogsByMonthAndYear() {
-        const logFiles = this.fileTransport.getLogFilesInDirectory();
-        const groupedLogs = {};
-
-        logFiles.forEach((logFile) => {
-            const logFilePath = path.join(loggerConfig.filePath, logFile);
-            const stats = fs.statSync(logFilePath);
-            const fileDate = new Date(stats.mtime);
-
-            // Extract month and year (e.g., "2023-10")
-            const monthYear = `${fileDate.getFullYear()}-${(
-                fileDate.getMonth() + 1
-            )
-                .toString()
-                .padStart(2, "0")}`;
-
-            if (!groupedLogs[monthYear]) {
-                groupedLogs[monthYear] = [];
-            }
-
-            // Read and append log content to the groupedLogs
-            const logContent = fs.readFileSync(logFilePath, "utf8");
-            groupedLogs[monthYear].push(logContent);
-        });
-
-        // Combine and save logs for each month and year
-        for (const [monthYear, logs] of Object.entries(groupedLogs)) {
-            const combinedLogs = logs.join("\n\n");
-            const combinedFileName = path.join(
-                loggerConfig.filePath,
-                `${monthYear}.txt`
-            );
-            fs.writeFileSync(combinedFileName, combinedLogs);
+      } catch (error) {
+        this.startLogWriterTimer();
+        if (error instanceof LogSaveException) {
+          this.consoleTransport.log(
+            `Logger::startLogWriterTimer() Exception during saving the logs => ${error} => ${error.parentError}`,
+            logLevels.ERROR,
+          );
+        } else {
+          this.consoleTransport.log(
+            `Logger::startLogWriterTimer() Exception occurred that isn't LogSaveException, possible exception leak => ${error}`,
+            logLevels.ERROR,
+          );
         }
-
-        // Delete individual log files after combining
-        logFiles.forEach((logFile) => {
-            const logFilePath = path.join(loggerConfig.filePath, logFile);
-            fs.unlinkSync(logFilePath);
-        });
-    }
+      }
+    }, loggerConfig.timerWriterInterval);
+  }
 }
-
 module.exports = new Logger();
--- src/processes/initialization/initialization.js
+++ src/processes/initialization/initialization.js
@@ -6,10 +6,10 @@ const Auditor = require("../../processes/activity_checker/auditor");
 
 // Exceptions
 const {
-    GuildNotFoundException,
+  GuildNotFoundException,
 } = require("../../../exceptions/initialization/GuildNotFoundException");
 const {
-    ChannelNotFoundException,
+  ChannelNotFoundException,
 } = require("../../../exceptions/initialization/ChannelNotFoundException");
 
 // Constants
@@ -18,78 +18,71 @@ const { logLevels } = require("../../constants/loggerConsts");
 const { reactionEmojis } = require("../../constants/serverConstants");
 
 module.exports = {
-    /**
-     * Clears the terminal and begins the logger
-     * @param {*} client
-     */
-    startLogger: (client) => {
-        console.clear();
-        Logger.startLogWriterTimer();
-        // Logger.startLogCombinerTimer(); //TODO Get this working
-        Logger.log(`Logged in as ${client.user.tag}`, logLevels.INFO);
-    },
+  /**
+   * Clears the terminal and begins the logger
+   * @param {*} client
+   */
+  startLogger: (client) => {
+    console.clear();
+    Logger.startLogWriterTimer();
+    Logger.log(`Logged in as ${client.user.tag}`, logLevels.INFO);
+  },
 
-    startAuditor: () => {}, // Fill in
+  startAuditor: () => {}, //TODO Fill in
 
-    /**
-     * Checks to ensure that the guild and channel id provided is valid, throws errors if not
-     * @param {*} client
-     * @return {*} guild, channel
-     */
-    checkIds: async (client) => {
-        let guild, channel;
-        try {
-            guild = await guildIdCheck(client, constantIds.serverID);
-            channel = await channelIdCheck(
-                guild,
-                roleConfigIds.roleSelectionChannelID
-            );
-        } catch (err) {
-            if (err instanceof GuildNotFoundException) {
-                Logger.log(err.message, logLevels.FATAL);
-            } else if (err instanceof ChannelNotFoundException) {
-                Logger.log(err.message, logLevels.FATAL);
-            } else {
-                Logger.log(
-                    `client.on() Unknown error occured => ${err}`,
-                    logLevels.FATAL
-                );
-            }
-            throw err;
-        }
-        return guild, channel;
-    },
-
-    /**
-     * Ensures that the message exists for others to react on
-     * @param {*} channel
-     */
-    //TODO In the event the message doesn't exist, the bot should make it. Create this as a util file
-    reactionInitialization: async (channel) => {
-        const message = await channel.messages.fetch(
-            roleConfigIds.setRoleMessageID
+  /**
+   * Checks to ensure that the guild and channel id provided is valid, throws errors if not
+   * @param {*} client
+   * @return {*} guild, channel
+   */
+  checkIds: async (client) => {
+    let guild, channel;
+    try {
+      guild = await guildIdCheck(client, constantIds.serverID);
+      channel = await channelIdCheck(
+        guild,
+        roleConfigIds.roleSelectionChannelID,
+      );
+    } catch (err) {
+      if (err instanceof GuildNotFoundException) {
+        Logger.log(err.message, logLevels.FATAL);
+      } else if (err instanceof ChannelNotFoundException) {
+        Logger.log(err.message, logLevels.FATAL);
+      } else {
+        Logger.log(
+          `client.on() Unknown error occured => ${err}`,
+          logLevels.FATAL,
         );
+      }
+      throw err;
+    }
+    return guild, channel;
+  },
 
-        //TODO Create Emoji wrapper class? Maybe a type of Enum?
-        for (const emoji of Object.values(reactionEmojis)) {
-            Logger.log(
-                `Checking for ${emoji} emoji on message: ${message.id}.`
-            );
-            if (!emojiExistOnMessage(message, emoji, (selfCheck = true))) {
-                await message.react(emoji).catch((error) => {
-                    Logger.log(
-                        `Error when trying to put ${emoji} on message: ${message.id} => ${error}.`,
-                        logLevels.ERROR
-                    );
-                });
-                Logger.log(
-                    `Successfully put ${emoji} on message: ${message.id}.`
-                );
-            } else {
-                Logger.log(
-                    `${emoji} already exists on message: ${message.id}.`
-                );
-            }
-        }
-    },
+  /**
+   * Ensures that the message exists for others to react on
+   * @param {*} channel
+   */
+  //TODO In the event the message doesn't exist, the bot should make it. Create this as a util file
+  reactionInitialization: async (channel) => {
+    const message = await channel.messages.fetch(
+      roleConfigIds.setRoleMessageID,
+    );
+
+    //TODO Create Emoji wrapper class? Maybe a type of Enum?
+    for (const emoji of Object.values(reactionEmojis)) {
+      Logger.log(`Checking for ${emoji} emoji on message: ${message.id}.`);
+      if (!emojiExistOnMessage(message, emoji, (selfCheck = true))) {
+        await message.react(emoji).catch((error) => {
+          Logger.log(
+            `Error when trying to put ${emoji} on message: ${message.id} => ${error}.`,
+            logLevels.ERROR,
+          );
+        });
+        Logger.log(`Successfully put ${emoji} on message: ${message.id}.`);
+      } else {
+        Logger.log(`${emoji} already exists on message: ${message.id}.`);
+      }
+    }
+  },
 };
