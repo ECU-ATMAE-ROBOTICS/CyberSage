c73f1cb4a295d36b5d540619b627c84c80109173
JarredTD <jarred.desrosiers@yahoo.com>
2024-04-13 16:15:38 -0400

    docs(comments): Reorganize comments & seperators

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
-//* Imports
 // <------------------------------->
+//* Imports
+
 // Thirdparty
-const Config = require("./ENV/secrets.json");
 const { Client, GatewayIntentBits } = require("discord.js");
 
 // Internal
+const Config = require("./ENV/secrets.json");
+
 const {
-    startLogger,
-    checkIds,
-    reactionInitialization,
+  startLogger,
+  checkIds,
+  reactionInitialization,
 } = require("./src/processes/initialization/initialization");
 
 // Event Handlers
 const {
-    emojiAddRole,
+  emojiAddRole,
 } = require("./src/events/reaction/reaction_add/messageReactionAdd.js");
 const {
-    emojiRemoveRole,
+  emojiRemoveRole,
 } = require("./src/events/reaction/reaction_remove/messageReactionRemove");
 
-//* Client Setup
 // <------------------------------->
+//* Client Setup
+
 //TODO Reduce unnecessary intents and partials
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildPresences,
-    ],
-    partials: [
-        "MESSAGE",
-        "CHANNEL",
-        "REACTION",
-        "USER",
-        "GUILD_MEMBER",
-        "GUILD_PRESENCES",
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildPresences,
+  ],
+  partials: [
+    "MESSAGE",
+    "CHANNEL",
+    "REACTION",
+    "USER",
+    "GUILD_MEMBER",
+    "GUILD_PRESENCES",
+  ],
 });
 
+// <------------------------------->
 //* Initialization
-// <------------------------------->
+
 client.on("ready", async () => {
-    startLogger(client);
+  startLogger(client);
 
-    let guild,
-        channel = await checkIds(client);
+  let guild,
+    channel = await checkIds(client);
 
-    reactionInitialization(channel);
+  reactionInitialization(channel);
 });
 
-//* Event Handlers
 // <------------------------------->
+//* Event Handlers
+
 //? These event handlers are entry points. We should include logic to seperate which handler to use for which events.
 //? This could get sticky. We should consider a modular and clean way to handle this if more processes are added.
 // Register event handlers
 client.on("messageReactionAdd", (reaction, user) => {
-    emojiAddRole(client, reaction, user);
+  emojiAddRole(client, reaction, user);
 });
 
 client.on("messageReactionRemove", (reaction, user) => {
-    emojiRemoveRole(client, reaction, user);
+  emojiRemoveRole(client, reaction, user);
 });
 
-//* Bot Start
 // <------------------------------->
+//* Bot Start
+
 client.login(Config.token);
